9469c4df0a234234dcecd8466836a71c7732b870
Philip <phil@nevie.com>
2021-06-09 12:09:30 +0300

    getting ready for Pull Resquest proccess

--- app/src/main/java/com/philipcutting/restaurantapp/models/Order.kt
+++ app/src/main/java/com/philipcutting/restaurantapp/models/Order.kt
@@ -4,7 +4,9 @@ import java.time.Instant
 
 data class Order(
     val menuItems: MutableList<MenuItem>,
+
     val time_in_minutes: Double = 0.0,
     val timeInitiated: Instant? = null,
+
     val orderPickedUp: Boolean = false
 )
